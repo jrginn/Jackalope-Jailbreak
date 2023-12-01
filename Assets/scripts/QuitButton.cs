e0f2472873ca7d3592e6c87f0e21e2c17a595e3d
Eric Burkholder <ericwb@email.sc.edu>
2023-12-01 16:21:30 -0500

    Mini game end screen prefab, pause menu works, small changes to dart scene

---
+++ Assets/scripts/QuitButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class QuitButton : MonoBehaviour
+{
+    private Button quitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        quitButton = GetComponent<Button>();
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void QuitGame()
+    {
+        GameManager.Instance.QuitGame();
+    }
+}
