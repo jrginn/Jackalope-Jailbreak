30c125d24b21b314186621b25ea4db85f53afe10
jrginn <jrginn47@gmail.com>
2023-12-01 14:20:03 -0500

    timer persists between scenes

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -8,41 +8,68 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
 
-    public float seconds;
-    private TextMeshProUGUI timeText;
+    #region Singleton
+    private static Timer _instance;
+    public static Timer Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("Timer is NULL");
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+            Destroy(this.gameObject);
+        else
+            _instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    #endregion
+
+    public float seconds = 0;
+    public TextMeshProUGUI timeText;
 
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(gameObject);
         timeText = GameObject.FindGameObjectWithTag("TimerText").GetComponent<TextMeshProUGUI>();
-        print("seconds: " + seconds);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(seconds > 0)
+
+        if (seconds < 600)
         {
-            seconds -= Time.deltaTime;
+            seconds += Time.deltaTime;
         }
         else
         {
-            seconds = 0;
+            seconds = 600;
             print("L");
         }
         if (SceneManager.GetActiveScene().name.Equals("MainScene"))
         {
+            if (timeText == null)
+            {
+                timeText = GameObject.FindGameObjectWithTag("TimerText").GetComponent<TextMeshProUGUI>();
+            }
             DisplayTime(seconds);
         }
     }
 
     void DisplayTime(float timeToDisplay)
     {
-        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        float milliSeconds = (timeToDisplay % 1) * 1000;
+        float minutes = 9 - Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = 59 - Mathf.FloorToInt(timeToDisplay % 60);
+        float milliSeconds = 999 - (timeToDisplay % 1) * 1000;
         timeText.text = string.Format("{0:00}:{1:00}:{2:000}", minutes, seconds, milliSeconds);
     }
 }
